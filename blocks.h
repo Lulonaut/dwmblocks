7752c546fbe154c1bb6a5733b0ff7ef6b662ad95
Lulonaut <lulonaut@tutanota.de>
2023-02-03 13:51:36 +0100

    volume/ram

--- blocks.h
+++ blocks.h
@@ -1,7 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "ncspot_info",     10,      0},
+	{"", "ncspot_info",                        10,      0},
+	{"", "get_volume",                     		5,      0},
+	{"", "get_ram_usage",                     	5,      0},
+	{"", "status",                             10,      0},
 	{"", "date +'%d.%m.%Y %T'",					1,		0},
 };
 
